72c56b0f2ed713b17d26e74e16fa589fb7a8a0b8
Tony Li <tonyli@tonys-mbp.home>
2022-04-10 22:42:17 +1000

    project 2 text file signum population

--- Project2/text_analysis.r
+++ Project2/text_analysis.r
@@ -1,18 +1,36 @@
-library(readxl);
 library(tidyverse);
-library(stringr);
+library(readxl);
 # ===============================================================
 # CREATING THE TEXT LOOKUP TABLE
 # ===============================================================
-inscription_lookup_table = data.frame(matrix(ncol = 2, nrow = 0))
-table_column_names = c("Signum", "text")
-colnames(inscription_lookup_table) = table_column_names
+inscription_signum_lookup_table = data.frame(matrix(ncol = 1, nrow = 0))
 
-print(inscription_lookup_table)
-print("hello")
+inscription_signum_lookup_table_col_names = c("Signum")
+colnames(inscription_signum_lookup_table) = inscription_signum_lookup_table_col_names
 
 # ===============================================================
-# READING INSCRIPTIONS DATA FILE
+# READING MASTER DATA FILE
+# ===============================================================
+
+# Read in master spread sheet
+master_dataset = read_excel("Project2/Data/Arch_master_spreadsheet.xls", sheet = "rundata", col_types = NULL, na = "")
+master_signum = master_dataset$Signum
+str(master_signum)
+
+# Create a signum lookup table
+master_signum_lookup_table = data.frame(matrix(ncol = 1, nrow = 0))
+master_signum_lookup_col_names = c("Signum")
+colnames(master_signum_lookup_table) = master_signum_lookup_col_names
+# Testing for existance of master_lookup_table
+print(master_signum_lookup_table)
+str(master_signum_lookup_table)
+
+# Bind the master signums into signum lookup table
+master_signum_lookup_table = cbind(master_signum)
+str(master_signum_lookup_table)
+
+# ===============================================================
+# READING SIGNUM OF INSCRIPTIONS DATA FILE
 # ===============================================================
 inscription_file = file(
     description = "Project2/Data/Arch_english_inscriptions.txt",
@@ -20,20 +38,27 @@ inscription_file = file(
     blocking = TRUE
 );
 
+
+
 repeat {
     single_line = readLines(inscription_file, n = 1);
     sig_num_match = str_match(single_line, "[^ ]* [^ ]* [^ ]*")
+    inscription_signum_lookup_table = rbind(inscription_signum_lookup_table, sig_num_match)
     result = "result: "
     string_print = paste(result, sig_num_match, sep = " ")
     print(string_print)
     if (identical(single_line, character(0))) {
         break;
     }
+    
     print(single_line);
-
 }
-
 close(inscription_file);
 rm(inscription_file);
+
+print(inscription_signum_lookup_table)
+str(inscription_signum_lookup_table)
+
+
 
 
