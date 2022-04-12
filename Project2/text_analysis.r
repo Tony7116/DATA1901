a8a3ac0f37615a3d98dc3dd2c34f2471e0b2332e
Tonyatdoshii <tony.li@doshii.io>
2022-04-13 08:41:57 +1000

    completed text data cleaning

--- Project2/text_analysis.r
+++ Project2/text_analysis.r
@@ -5,12 +5,26 @@ library(readxl);
 # ===============================================================
 inscription_lookup_table = data.frame(matrix(ncol = 3, nrow = 0))
 
-inscription_lookup_table_col_names = c("Signum", "text", "dates")
+inscription_lookup_table_col_names = c("Signum", "text", "tally")
 colnames(inscription_lookup_table) = 
     inscription_lookup_table_col_names
 
 print(inscription_lookup_table)
 str(inscription_lookup_table)
+
+# ===============================================================
+# CREATING THE DATE LOOKUP TABLE
+# ===============================================================
+date_lookup_table = data.frame(matrix(ncol = 2, nrow = 0))
+print(date_lookup_table)
+str(date_lookup_table)
+
+# ===============================================================
+# CREATING THE INSCRIPTION TEXT-DATE LOOKUP TABLE
+# ===============================================================
+text_date_lookup_table = data.frame(matrix(ncol = 3, nrow = 0))
+print(text_date_lookup_table)
+str(text_date_lookup_table)
 
 # ===============================================================
 # CREATING THE Signum LOOKUP TABLE
@@ -24,33 +38,32 @@ colnames(master_signum_lookup_table) = master_signum_lookup_col_names
 print(master_signum_lookup_table)
 str(master_signum_lookup_table)
 
-
-# ===============================================================
-# CREATING THE inscription text LOOKUP TABLE
-# ===============================================================
-# Create a inscription text lookup table
-inscription_text_lookup_table = data.frame(matrix(ncol = 1, nrow = 0))
-inscription_text_lookup_col_names = c("text")
-colnames(inscription_text_lookup_table) = inscription_text_lookup_col_names
-# Testing for existance of master_lookup_table
-print(inscription_text_lookup_table)
-str(inscription_text_lookup_table)
-
 # ===============================================================
 # READING MASTER DATA FILE
 # ===============================================================
 
 # Read in master spread sheet
-master_dataset = read_excel("Project2/Data/Arch_master_spreadsheet.xls", sheet = "rundata", col_types = NULL, na = "")
-master_signum = master_dataset$Signum
-str(master_signum)
+master_dataset = read_excel(
+    "Project2/Data/Arch_master_spreadsheet.xls",
+    sheet = "rundata",
+    col_types = NULL,
+    na = ""
+)
 
-# Bind the master signums into signum lookup table
-master_signum_lookup_table = cbind(master_signum)
-str(master_signum_lookup_table)
+signum = master_dataset$Signum
+str(signum)
+dates = master_dataset$Stilgruppering
+print(dates)
+str(dates)
+
+
+date_lookup_table = cbind("signum" = signum, "dates" = dates)
+str(date_lookup_table)
+date_lookup_table
 
 # ===============================================================
-# READING SIGNUM OF INSCRIPTIONS DATA FILE
+# READING AND POPULATING SIGNUM OF INSCRIPTIONS TEXT FILE TO
+# INSCRIPTION LOOKUP TABLE.
 # ===============================================================
 inscription_file = file(
     description = "Project2/Data/Arch_english_inscriptions.txt",
@@ -58,35 +71,92 @@ inscription_file = file(
     blocking = TRUE
 );
 
+bible_file = read.delim("Project2/Data/bible.txt", header = FALSE);
+print(bible_file[2, 1])
 
-    single_line = readLines(inscription_file, n = 1);
+repeat {
+    # Reading each line of text in the inscription file
+    single_line = readLines(inscription_file, n = 1)
+    master_signum_lookup = master_signum_lookup_table[1, ]
 
     sig_num_match = str_match(single_line, "[^ ]* [^ ]* [^ ]*")
+    print(sig_num_match)
     single_text_line = str_remove(single_line, "[^ ]* [^ ]* [^ ]* ")
-    cleaned_single_text_line = str_remove_all(single_text_line, "([.][.][.][ ])|([ ][.][.][.])")
-
-    print(single_text_line)
+    cleaned_single_text_line = 
+        str_remove_all(single_text_line, "([.][.][.][ ])|([ ][.][.][.])")
     print(cleaned_single_text_line)
-    print(final_single_text_line)
 
-    inscription_signum_lookup_table = 
-        rbind(inscription_signum_lookup_table, sig_num_match)
+    split_text_line = str_split(cleaned_single_text_line, " ")
+    split_text_line_vector = unlist(split_text_line)
+    print(split_text_line)
+    print(split_text_line_vector[1])
 
-    result = "result: "
-    string_print = paste(result, sig_num_match, sep = " ")
-     print(string_print)
+    count = 0
+    for (i in 1:lengths(split_text_line)) {
+        tolower(split_text_line)
+        for (j in 1:2) {
+            if (split_text_line_vector[i] == bible_file[j, 1]) {
+                count = count + 1
+            }
+        }
+    }
 
-    text_result = "text: "
-    text_print = paste(text_result, string_print, sep = " ")
+    print(count)
 
-    print(text_print)
+    inscription_lookup_table = 
+        rbind(
+            inscription_lookup_table,
+            c(sig_num_match, cleaned_single_text_line, count))
+
     if (identical(single_line, character(0))) {
         break;
     }
-    print(single_line);
+}
+
+
 
 close(inscription_file);
 rm(inscription_file);
 
-print(inscription_signum_lookup_table)
-str(inscription_signum_lookup_table)
+print(inscription_lookup_table)
+str(inscription_lookup_table)
+colnames(inscription_lookup_table) <- c("signum", "text", "tally")
+str(inscription_lookup_table)
+
+# ===============================================================
+# REMOVING UNWANTED TEXT ROWS IN INSCRIPTION TABLE
+# ===============================================================
+inscription_lookup_table =
+    inscription_lookup_table[!inscription_lookup_table$text == "", ]
+inscription_lookup_table =
+    inscription_lookup_table[!inscription_lookup_table$text == "°", ]
+
+str(inscription_lookup_table)
+
+# ===============================================================
+# JOINING THE DATE LOOKUP TABLE WITH THE TEXT LOOKUP TABLE
+# ===============================================================
+
+text_date_lookup_table = merge(
+    x = inscription_lookup_table,
+    y = date_lookup_table,
+    by = "signum"
+
+)
+print(text_date_lookup_table)
+str(text_date_lookup_table)
+
+text_date_lookup_table$tally = as.numeric(text_date_lookup_table$tally)
+print(text_date_lookup_table$tally)
+
+# Removing the rows which have 0 as their tally
+text_date_lookup_table = subset(
+    text_date_lookup_table,
+    text_date_lookup_table$tally > 0
+)
+
+print(text_date_lookup_table)
+str(text_date_lookup_table)
+ggplot(text_date_lookup_table,
+    aes(x = tally)
+) + geom_bar()
